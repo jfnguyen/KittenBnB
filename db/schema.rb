0e0312570a0cd426858c0b129eac347c7ade1e7c
Joseph Nguyen <joseph.francis.nguyen@gmail.com>
2016-04-19 16:42:17 -0700

    prefer pg over sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160419044755) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "listings", force: :cascade do |t|
     t.integer  "host_id",         null: false
     t.float    "latitude",        null: false
